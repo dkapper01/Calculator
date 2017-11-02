82e5068f33d8967aaf422c9e8cf070d54a50906c
Daniel Kapper <dkapper01@gmail.com>
2017-11-02 14:33:09 -0400

    enter nums and clear nums

--- calc.js
+++ calc.js
@@ -1,1 +1,16 @@
-console.log("working");
+console.log("calculator working...");
+
+
+
+function displayOnScreen(x) {
+  var box = document.getElementById("display");
+  var arr = [];
+  if(x === 'c') {
+    console.log("=c");
+  } else {
+    box.value += x;
+    arr.push(box.value);
+  }
+  box.value = arr;  
+}
+// display_on_screen();

ebfd9c8bca28792dec8670fc0b0ef45b67063ecf
Daniel Kapper <dkapper01@gmail.com>
2017-11-12 20:27:10 -0500

    starting a new

--- calc.js
+++ calc.js
@@ -1,16 +1,33 @@
 console.log("calculator working...");
 
+var screen = document.getElementById("display");
+var topScreen = document.getElementById("upper_display");
+var view = 0;
+var num = 0; 
 
 
 function displayOnScreen(x) {
-  var box = document.getElementById("display");
-  var arr = [];
-  if(x === 'c') {
-    console.log("=c");
-  } else {
-    box.value += x;
-    arr.push(box.value);
-  }
-  box.value = arr;  
+  num = screen.value += x; 
+  
+  // var arr = [];
+  // clearAll(x);
+  // if (isNaN(x) === false){
+  //   screen.value += x;
+  //   arr.push(screen.value);
+  // } else {
+  //   console.log('green');
+  // }
+  // return screen.value = arr;
 }
-// display_on_screen();
+
+function mathOperator(y) {
+  topScreen.value += num + " " + y;
+}
+
+function clearAll(x) {
+  if(x === 'ec')
+    // topScreen.value = '';
+    // screen.value = '';
+    return;
+
+}

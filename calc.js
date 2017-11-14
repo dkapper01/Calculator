64462f6a8da44618e0761d8be1d174190526a39f
Daniel Kapper <dkapper01@gmail.com>
2017-11-14 16:02:35 -0500

    add digit method

--- calc.js
+++ calc.js
@@ -1,33 +1,18 @@
 console.log("calculator working...");
 
-var screen = document.getElementById("display");
-var topScreen = document.getElementById("upper_display");
-var view = 0;
-var num = 0; 
+var memory = '0'; 
+var current = 0; 
+var Operation = 0; 
+var MAXLENGTH = 3;
+
+document.getElementById('display').value = 0; 
 
 
-function displayOnScreen(x) {
-  num = screen.value += x; 
-  
-  // var arr = [];
-  // clearAll(x);
-  // if (isNaN(x) === false){
-  //   screen.value += x;
-  //   arr.push(screen.value);
-  // } else {
-  //   console.log('green');
-  // }
-  // return screen.value = arr;
-}
-
-function mathOperator(y) {
-  topScreen.value += num + " " + y;
-}
-
-function clearAll(x) {
-  if(x === 'ec')
-    // topScreen.value = '';
-    // screen.value = '';
-    return;
-
+function addDigit(dig) {       
+  if(current === 0) {
+    current = dig; 
+  } else {
+    current += dig; 
+  }
+  document.getElementById('display').value = current;
 }

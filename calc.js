9b00dd64cb6df27a4d311a7252afa9583905d7db
Daniel Kapper <dkapper01@gmail.com>
2017-11-21 12:24:18 -0500

    Go back to original calc

--- calc.js
+++ calc.js
@@ -9,20 +9,10 @@ document.getElementById('display').value = 0;
 
 
 function addDigit(dig) {    
-<<<<<<< HEAD
   if( eval(current) === 0) {
     current = dig; 
   } else {
     current += dig; 
-=======
-  if(current.length < MAXLENGTH) {
-    if( eval(current) === 0) {
-      current = dig; 
-    } else {
-      current += dig; 
-    }
-    document.getElementById('display').value = current;
->>>>>>> parent of 9059435... Got rid of input field
   }
   document.getElementById('display').value = current;
 } 

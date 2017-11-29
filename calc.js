84dbbceab0e80e75e25e6b21b867c418b722411d
Daniel Kapper <dkapper01@gmail.com>
2017-11-29 13:26:15 -0500

    ce btn working

--- calc.js
+++ calc.js
@@ -21,3 +21,10 @@ function power() {
   x = eval(x*x); 
   display.value = x; 
 }
+
+function clearInput() {
+  var num = display.value; 
+  var len = num.length - 1; 
+  num = num.substring(0,len); 
+  display.value = num; 
+}
